a5c1e8eea4104ec5bca2d4088fb6696036fea0c9
rafael <rafaelmagalhaes93@gmail.com>
2020-03-03 13:25:32 +0000

    base actions setup, fetch list of cars and single car detail

---
+++ src/redux/actions.js
@@ -0,0 +1,62 @@
+export const REQUEST_CARS = "REQUEST_CARS";
+export const RECEIVE_CARS = "RECEIVE_CARS";
+
+export const REQUEST_SINGLE_CAR = "REQUEST_SINGLE_CAR";
+export const RECEIVE_SINGLE_CAR = "RECEIVE_SINGLE_CAR";
+
+let api_url = 'https://warm-dawn-92320.herokuapp.com/models';
+
+function requestCars() {
+    return {
+        type: REQUEST_CARS
+    };
+}
+
+
+function receiveCars(data) {
+    return {
+        type: RECEIVE_CARS,
+        cars: data
+    };
+}
+
+function requestSingleCar() {
+    return {
+        type: REQUEST_SINGLE_CAR
+    };
+}
+
+function receiveSingleCar(data) {
+    return {
+        type: RECEIVE_SINGLE_CAR,
+        car: data
+    };
+}
+
+export function fetchCars() {
+    return function (dispatch) {
+        dispatch(requestCars());
+
+        return fetch(api_url)
+            .then(
+                response => response.json(),
+                error => console.log("An error occurred.", error)
+            )
+            .then(json => {
+                dispatch(receiveCars(json))
+            });
+    };
+}
+
+export function fetchSingleCar(id) {
+    return function (dispatch) {
+        dispatch(requestSingleCar());
+        return fetch(`${api_url}/${id}`)
+            .then(
+                response => response.json(),
+                error => console.log("An error occurred.", error)
+            )
+            .then(json => dispatch(receiveSingleCar(json)));
+
+    };
+}
