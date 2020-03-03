cbdd628cd01241438e660bef4f888e792e426e9e
rafael <rafaelmagalhaes93@gmail.com>
2020-03-03 18:28:56 +0000

    simple test check inital redux state

---
+++ src/redux/reducer.spec.js
@@ -0,0 +1,22 @@
+import cars from './reducer'
+
+describe('cars Reducers', () => {
+    const initState = {
+        isFetching: false,
+        cars: [],
+        car: {},
+    };
+
+    it('returns the initial state when actions is not passed', () => {
+        const reducer = cars(undefined, {});
+        expect(reducer).toEqual(initState)
+    });
+    it('isFetching should be true when action REQUEST_CARS is called', () => {
+        const reducer = cars(initState, {type: "REQUEST_CARS"});
+        expect(reducer).toEqual({
+            isFetching: true,
+            cars: [],
+            car:{}
+        });
+    });
+});
